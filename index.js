4c5883af4d442455d16bfc73417d2ab39534093d
MatthieuI <58854860+MatthieuI@users.noreply.github.com>
2026-02-05 20:33:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,393 @@
+const list = [
+    {
+        path: "./audio/about.mp3",
+        text: "J'suis à bout",
+        source: "Meloo"
+    },
+
+    {
+        path: "./audio/appelpapa.mp3",
+        text: "J'vais appeler ton papa",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/apte.mp3",
+        text: "Apte à pas faire de dinguerie",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/arretebetises.mp3",
+        text: "Arrête tes bêtises",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/arreteuh.mp3",
+        text: "Arrêteeeeuh",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/audace.mp3",
+        text: "L'audace",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/backflip.mp3",
+        text: "Je fais des backflip (loupé)",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/bebete.mp3",
+        text: "Je fais bébête",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/bitetrans.mp3",
+        text: "Bite de trans",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/blaster.mp3",
+        text: "Blaster",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/bouteille.mp3",
+        text: "La bouteille ou rien",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/br.mp3",
+        text: "Me suis pas BR",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/cettespeed.mp3",
+        text: "Cette speed",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/comparetonpaf.mp3",
+        text: "Compare ton paf",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/competitif.mp3",
+        text: "Les prods compétitifs",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/crackhead.mp3",
+        text: "Ra la la, le crackhead",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/dernierechance.mp3",
+        text: "Une dernière chance",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/deuxt.mp3",
+        text: "Matthieu, avec deux T",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/doigtTP.mp3",
+        text: "Téléportation de doigt",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/donnezMoi.mp3",
+        text: "Donnez moi des taz",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/dresserlepaf.mp3",
+        text: "Dresser le paf",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/especedechienne.mp3",
+        text: "Espèce de chienne",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/foirfouille.mp3",
+        text: "La Foir'Fouille",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/four.mp3",
+        text: "Le four",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/four2.mp3",
+        text: "Le four, 2ème round",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/harr.mp3",
+        text: "C'est harr",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/innover.mp3",
+        text: "L'innovation de Lowqiie",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/inrecup.mp3",
+        text: "INrécupérable",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/jungle.mp3",
+        text: "Petit jungle",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/jvaisvenirtemanger.mp3",
+        text: "J'vais venir te manger",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/ledroitmoi.mp3",
+        text: "J'ai pas le droit, MOI ?",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/lestaz.mp3",
+        text: "Les taz vont partir",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/effetL.mp3",
+        text: "Effets de L, By Lowqiie",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/luisa.mp3",
+        text: "Luisa, sauve moi !",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/mailleurcrackhead.mp3",
+        text: "Le meilleur crackhead",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/microOndes.mp3",
+        text: "Je les fais au micro-ondes",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/onsenbranle.mp3",
+        text: "ON S'EN BRANLE",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/onvaautb.mp3",
+        text: "On va au TB",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/onvasedetendre.mp3",
+        text: "On va se détendre",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/onvaseregaler.mp3",
+        text: "On va se régaler",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/papafouetter.mp3",
+        text: "Il va te fouetter",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/pasgay.mp3",
+        text: "J'suis pas gay",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/pausesoiree.mp3",
+        text: "Pause de soirée tech",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/planning.mp3",
+        text: "Planning",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/plusintelloquelaukiie.mp3",
+        text: "Plus intelligent",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/pokecrack.mp3",
+        text: "Pokécrack !!!",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/quemoiquipeutsavoir.mp3",
+        text: "C'est moi qui sais",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/serieuxouquoi.mp3",
+        text: "Sérieux ou quoi ?!",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/tagueule.mp3",
+        text: "Ta gueule",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/tropmechant.mp3",
+        text: "Trop méchants",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/trucdemerde.mp3",
+        text: "Ce truc de merde",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/unetdemioudeux.mp3",
+        text: "Un et demi ou deux...",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/valabas.mp3",
+        text: "Va là bas !",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/couteau.mp3",
+        text: "Le couteau le plus aiguisé",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/encules.mp3",
+        text: "Vous êtes des enculés",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/gropetou.mp3",
+        text: "Gro Prout",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/house.mp3",
+        text: "C'est de la house",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/jvousparleplus.mp3",
+        text: "J'vous parle plus",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/monprecieux.mp3",
+        text: "Mon précieux",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/retournerdormir.mp3",
+        text: "Retourner dormir",
+        source: "Meloo"
+    },
+    {
+        path: "./audio/ROTOTO.mp3",
+        text: "Rototo",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/soireechampi.mp3",
+        text: "Soirée Champi",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/transphobe.mp3",
+        text: "Toujours transphobe",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/voisine.mp3",
+        text: "On non pas la voisine",
+        source: "Kayou"
+    },
+    {
+        path: "./audio/wowowo.mp3",
+        text: "Ouah wowow",
+        source: "Kayou"
+    },
+];
+
+function play(n) {
+    const playing = isPlaying();
+    if (playing) {
+        document.querySelectorAll("audio")[playing].pause();
+        document.querySelectorAll("audio")[playing].currentTime = 0;
+    }
+    document.querySelectorAll("audio")[n].play();
+}
+
+function isPlaying() {
+    for (let i = 0; i < document.querySelectorAll("audio").length; i++) {
+        if (!document.querySelectorAll("audio")[i].paused) {
+            return i;
+        }
+    }
+    return false;
+}
+
+function createCard(i) {
+    let player = document.createElement("div");
+    let playerButton = document.createElement("button");
+    playerButton.classList.add(list[i].source.toLowerCase());
+    player.appendChild(playerButton);
+    playerText = document.createElement("p");
+    playerText.textContent = list[i].text;
+    player.appendChild(playerText);
+    let playerAudio = document.createElement("audio");
+    let source = document.createElement("source");
+    source.src = list[i].path;
+    source.format = "mp3";
+    playerAudio.appendChild(source);
+    player.appendChild(playerAudio);
+    player.onclick = function () { play(i) };
+    main.appendChild(player);
+}
+
+const main = document.querySelector("main");
+for (let i = 0; i < list.length; i++) {
+    createCard(i);
+}
+
+document.querySelector("select").addEventListener("change", function (e) {
+    main.innerHTML = "";
+    if (e.target.value === "all") {
+        for (let i = 0; i < list.length; i++) {
+            createCard(i);
+        }
+    } else {
+        for (let i = 0; i < list.length; i++) {
+            if (e.target.value === list[i].source) {
+                createCard(i);
+            }
+        }
+    }
+})
+
+document.querySelector("input").addEventListener("keyup", function (e) {
+    main.innerHTML = "";
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].text.toLowerCase().includes(e.target.value)) {
+            createCard(i);
+        }
+    }
+})
